61d77d6209a4edc128d64a3fc8b9ec19bf277872
Eric Arnold <ef.github@ondea.com>
2015-04-02 14:11:20 -0700

    Added unfolded state (and temp re-folding demo)

--- samples/web_shell/scripts/resources/editor/js/launch.js
+++ samples/web_shell/scripts/resources/editor/js/launch.js
@@ -14,39 +14,47 @@
 
 
 var Fold;
+this.allowNextUnfold_ = false;
+
 window.onload = function() {
-    require.config({
-        baseUrl: "http://acejs.localhost"
-    });
-    var editor;
-    require(["ace/ace", "ace/edit_session/fold"], function (ace, fold) {
-        editor = ace.edit("editor");
-        Fold = fold.Fold;
-        // editor.setTheme("ace/theme/monokai");
-        // editor.getSession().setMode("ace/mode/javascript");
-          var event = new Event('ready');
-          event.initEvent('ready', false, false);
-          this.dispatchEvent(event);
+  require.config({
+      baseUrl: "http://acejs.localhost"
+  });
+  var editor;
+  require(["ace/ace", "ace/edit_session/fold"], function (ace, fold) {
+      editor = ace.edit("editor");
+      Fold = fold.Fold;
+      // editor.setTheme("ace/theme/monokai");
+      // editor.getSession().setMode("ace/mode/javascript");
+        var event = new Event('ready');
+        event.initEvent('ready', false, false);
+        this.dispatchEvent(event);
 
-          editor.focus();
-          editor.commands.addCommand({
-            name: 'saveFile',
-            bindKey: {
-              win: 'Ctrl-S',
-              mac: 'Command-S',
-              sender: 'editor|cli'
-            },
-            exec: (function(editor, args, request) {
-              var event = new Event('save');
-              event.initEvent('save', false, false);
-              event.target = this;
-              this.dispatchEvent(event);
-            }).bind(this)
-          });
+        editor.focus();
+        editor.commands.addCommand({
+          name: 'saveFile',
+          bindKey: {
+            win: 'Ctrl-S',
+            mac: 'Command-S',
+            sender: 'editor|cli'
+          },
+          exec: (function(editor, args, request) {
+            var event = new Event('save');
+            event.initEvent('save', false, false);
+            event.target = this;
+            this.dispatchEvent(event);
+          }).bind(this)
+        });
 
-          setupVisualization(editor);
-    });
-}
+        setupVisualization(editor);
+  });
+
+  document.addEventListener('mousedown', function(e) {
+    if (e.target.classList.contains('ace_fold')) {
+      this.allowNextUnfold_ = true;
+    }
+  }.bind(this));
+}.bind(this);
 
 if (window.opener && window.opener.onEditorWindowOpened) {
   window.opener.onEditorWindowOpened();
@@ -62,10 +70,10 @@ function getContents() {
   return session.getValue();
 }
 
-function setupVisualization(editor) {
-  editor.on('input', function() {
-    var Range=require("ace/range").Range;
+this.setupVisualization = function (editor) {
+  var Range = require("ace/range").Range;
 
+  editor.on('input', function() {
     editor.getSession().unfold(2, true);
     var content = editor.session.getValue()
     // var re = /\/\*\* @type \{([A-Za-z_$.]+)\} ?\*\/\n(\s*)var/g;
@@ -98,29 +106,54 @@ function setupVisualization(editor) {
         subType = "blank_type_annotation";
 
       }
-      var placeholder = " \u25B6 " + typeName + " \u2630 ";
+      // var placeholder = " \u25B6 " + typeName + " \u2630 ";
+      var placeholder = " \u25B6 " + typeName + " ";
       placeholder = new Fold(markerRange, placeholder)
               placeholder.subType = subType;
       editor.session.addFold(placeholder, markerRange);
       // editor.session.addMarker(markerRange, "ace_selected-word", "text");
     }
-
   });
 
   editor.session.addEventListener("changeFold", function(e) {
     console.log(e);
     if (e.action == "remove") {
-      e.preventDefault();
-      e.stopPropagation();
-      var range = e.data.range;
-      try {
-        editor.session.addFold(e.data, range);
-      } catch (e) {
+      if (this.allowNextUnfold_) {
+        this.allowNextUnfold_ = false;
+        if (e.data.subType == 'blank_type_annotation' || e.data.subType == 'type_annotation') {
+          var content = editor.session.getValue()
+          var startIndex = editor.session.getDocument().positionToIndex(e.data.start);
+          var endIndex = editor.session.getDocument().positionToIndex(e.data.end);
+          var annotationText = content.substr(startIndex, endIndex);
+          var re = /(@type \{)([^}]+)\}/;
+          m = re.exec(annotationText)
+          var introLength = m[1].length;
+          var typeName = m[2];
+          var annotationStartIndex = m.index + startIndex + introLength;
+          var annotationEndIndex = annotationStartIndex + typeName.length;
+          var startPosition = editor.session.getDocument().indexToPosition(annotationStartIndex)
+          var endPosition = editor.session.getDocument().indexToPosition(annotationEndIndex)
+          var markerRange = new Range(startPosition.row, startPosition.column, endPosition.row, endPosition.column);
+          var placeholder = " \u25C0 " + typeName + " ";
+          placeholder = new Fold(markerRange, placeholder)
+          placeholder.subType = 'unfolded_annotation';
+          editor.session.addFold(placeholder, markerRange);
+        } else {
+          this.setupVisualization(editor);
+        }
+      } else {
+        e.preventDefault();
+        e.stopPropagation();
+        var range = e.data.range;
+        try {
+          editor.session.addFold(e.data, range);
+        } catch (e) {
 
+        }
+        // window.prompt("Type");
+        return false;
       }
-      // window.prompt("Type");
-      return false;
     }
-  });
+  }.bind(this));
   editor.session.setValue("//hello\n  /** @type {string} */\n  var pathSpec = 'hello'")
-}
+}.bind(this);
