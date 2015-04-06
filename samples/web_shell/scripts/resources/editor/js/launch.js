751111789efd627f18e3ce707a39ccc3f2af67c1
Eric Arnold <ef.github@ondea.com>
2015-04-06 03:27:57 -0500

    Class and constructor now work (unfold on constructor doesn't work yet)

--- samples/web_shell/scripts/resources/editor/js/launch.js
+++ samples/web_shell/scripts/resources/editor/js/launch.js
@@ -35,14 +35,24 @@ var Substitutor = function(findRegex, reverseToken, subType,
 Substitutor.prototype.matchAll = function(content) {
   var Range = require("ace/range").Range;
 
-  while(m = this.findRegex_.exec(content)) {
-    var info = this.getFoldToken_(m.index, content, m);
-    var range = this.rangeFromIndexes_(info.start, info.end);
-    if (this.session_.getFoldsInRange(range).length == 0) {
-      this.createSubstitution(range, info.label);
-    }
-  }
+  while(this.findAndCreateSubstitution_(content, null));
 }
+
+Substitutor.prototype.findAndCreateSubstitution_ = function(content, substitutor) {
+  m = this.findRegex_.exec(content)
+  if (!m) return false;
+
+  var info = this.getFoldToken_(m.index, content, m);
+  var range = this.rangeFromIndexes_(info.start, info.end);
+  // var start = m.index;
+  // var range = this.rangeFromIndexes_(start, m[0].length + start - 1);
+
+  if (this.session_.getFoldsInRange(range).length == 0) {
+    return this.createSubstitution(range, info.label, substitutor);
+  }
+  return true;
+}
+
 
 Substitutor.prototype.getFoldToken_ = function(startIndex, content, match) {
   return {label: m[this.matchIndex_], start: startIndex,
@@ -70,7 +80,9 @@ Substitutor.prototype.createSubstitution = function(markerRange, value,
     fold.substitution = substitution ? substitution : new Substitution(this);
     this.session_.addFold(fold, markerRange);
     // this.session_.addMarker(markerRange, "ace_selected-word", "text");
+    return fold;
   }
+  return null;
 }
 
 // Substitutor.prototype.findTop_ = function(content) {}
@@ -93,12 +105,7 @@ Substitutor.prototype.expand = function(fold) {
     var row = fold.start.row;
     this.findRegex_.lastIndex = content.lastIndexOf(this.reverseToken_, endIndex);
     this.session_.removeFold(fold);
-    m = this.findRegex_.exec(content);
-    var info = this.getFoldToken_(m.index, content, m);
-    var range = this.rangeFromIndexes_(info.start, info.end);
-    // var start = m.index;
-    // var range = this.rangeFromIndexes_(start, m[0].length + start - 1);
-    this.createSubstitution(range, info.label, fold.substitution);
+    this.findAndCreateSubstitution_(content, fold.substitution);
 
     // m = this.findRegex_.exec(content)
     // var start = m.index;
@@ -167,7 +174,6 @@ ClassSubstitutor.prototype.getFoldToken_ = function(startIndex, content, match) 
   return {label: 'class ' + m[3] + " {", start: annotationStart, end: foldEnd};
 }
 
-
 ClassSubstitutor.prototype.getUnfoldedToken_ = function(annotationText) {
   var re = /(\* )(@constructor)/g; // \b.*\*\/\nvar )([a-zA-Z_$][a-zA-Z_$0-9]*)
   m = re.exec(annotationText)
@@ -176,6 +182,30 @@ ClassSubstitutor.prototype.getUnfoldedToken_ = function(annotationText) {
   var annotationStartIndex = m.index + introLength;
   return {label: varKeyword, start: annotationStartIndex,
       end: annotationStartIndex + varKeyword.length};
+}
+
+ClassSubstitutor.prototype.findAndCreateSubstitution_ = function(content, substitutor) {
+  var fold = Substitutor.prototype.findAndCreateSubstitution_.call(this,
+      content, substitutor);
+  if (!fold || fold === true) {
+    return fold;
+  }
+  var constructorRegex = /(var )([a-zA-Z_$][a-zA-Z_$0-9]*)( = function)(\([^{]*)/g
+  constructorRegex.lastIndex = this.session_.getDocument().positionToIndex(
+      fold.start);
+  m = constructorRegex.exec(content)
+  var start = m.index;
+  var label = m[2]
+  var range = this.rangeFromIndexes_(start, start + m[1].length + label.length +
+      m[3].length);
+  // var start = m.index;
+  // var range = this.rangeFromIndexes_(start, m[0].length + start - 1);
+
+  if (this.session_.getFoldsInRange(range).length == 0) {
+    return this.createSubstitution(range, label, substitutor);
+  }
+  return true;
+
 }
 
 /**
